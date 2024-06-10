d64d43189b5b7b45d539a0b584d879ecf52c0ddd
Semih Turan <semituran@gmail.com>
2024-06-10 10:11:40 +0300

    add eureka server name as kitapyurdum-service-registry

---
+++ kitapyurdum-service-registry/src/test/java/dev/patika/kitapyurdumserviceregistry/KitapyurdumServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.patika.kitapyurdumserviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KitapyurdumServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
